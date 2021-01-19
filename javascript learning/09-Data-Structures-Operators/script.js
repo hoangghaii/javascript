26e32ad7f7f5ebf808005f6343d8a1e93948f1a2
hoangghaii <46194425+hoangghaii@users.noreply.github.com>
2021-01-19 16:19:37 +0700

    .

--- javascript learning/09-Data-Structures-Operators/script.js
+++ javascript learning/09-Data-Structures-Operators/script.js
@@ -53,6 +53,159 @@ const restaurant = {
 	},
 };
 
+// // Working With Strings - Part 1
+const airline = "TAP Air Portugal";
+// const plane = "A320";
+
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+// console.log("B737"[0]);
+
+// console.log(airline.length);
+// console.log("B737".length);
+
+// console.log(airline.indexOf("r"));
+// console.log(airline.lastIndexOf("r"));
+// console.log(airline.indexOf("portugal"));
+
+// console.log(airline.slice(4));
+// console.log(airline.slice(4, 7));
+
+// console.log(airline.slice(0, airline.indexOf(" ")));
+// console.log(airline.slice(airline.lastIndexOf(" ") + 1));
+
+// console.log(airline.slice(-2));
+// console.log(airline.slice(1, -1));
+
+// const checkMiddleSeat = function (seat) {
+// 	// B and E are middle seats
+// 	const s = seat.slice(-1);
+// 	s === "B" || s === "E"
+// 		? console.log("You got the middle seat 😬")
+// 		: console.log("You got lucky 😎");
+// };
+
+// checkMiddleSeat("11B");
+// checkMiddleSeat("23C");
+// checkMiddleSeat("3E");
+
+// console.log(new String("jonas"));
+// console.log(typeof new String("jonas"));
+
+// console.log(typeof new String("jonas").slice(1));
+
+// // Working With Strings - Part 2
+// console.log(airline.toLowerCase());
+// console.log(airline.toUpperCase());
+
+// // Fix capitalization in name
+// const passenger = "jOnAS"; // Jonas
+// const passengerLower = passenger.toLowerCase();
+// const passengerCorrect =
+// 	passengerLower[0].toUpperCase() + passengerLower.slice(1);
+// console.log(passengerCorrect);
+
+// // Comparing emails
+// const email = "hello@jonas.io";
+// const loginEmail = "  Hello@Jonas.Io \n";
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+// console.log(trimmedEmail);
+
+// const normalizedEmail = lowerEmail.toLowerCase().trim();
+// console.log(normalizedEmail);
+// console.log(email === normalizedEmail);
+
+// // replacing
+// const priceGB = "288,97£";
+// const priceUS = priceGB.replace("£", "$").replace(",", ".");
+// console.log(priceUS);
+
+// const announcement =
+// 	"All passengers come to boarding door 23. Boarding door 23!";
+
+// console.log(announcement.replace("door", "gate"));
+// console.log(announcement.replaceAll("door", "gate"));
+// console.log(announcement.replace(/door/g, "gate"));
+
+// // Booleans
+// const plane = "Airbus A320neo";
+// console.log(plane.includes("Airbus"));
+// console.log(plane.includes("Boeing"));
+// console.log(plane.startsWith("Airb"));
+
+// if (plane.startsWith("Airbus") && plane.endsWith("neo")) {
+// 	console.log("Part of the NEW ARirbus family");
+// }
+
+// // Practice exercise
+// const checkBaggage = function (items) {
+// 	const baggage = items.toLowerCase();
+
+// 	baggage.includes("knife") || baggage.includes("gun")
+// 		? console.log("You are NOT allowed on board")
+// 		: console.log("Welcome aboard!");
+// };
+
+// checkBaggage("I have a laptop, some Food and a pocket Knife");
+// checkBaggage("Socks and camera");
+// checkBaggage("Got some snacks and a gun for protection");
+
+// Working With Strings - Part 3
+
+// Split and join
+// console.log("a+very+nice+string".split("+"));
+// console.log("Jonas Schmedtmann".split(" "));
+
+// const [firstname, lastName] = "Jonas Schmedtmann".split(" ");
+
+// const newName = ["Mr.", firstname, lastName.toUpperCase()].join(" ");
+// console.log(newName);
+
+// const capitalizeName = function (name) {
+// 	const names = name.split(" ");
+// 	const nameUpper = [];
+
+// 	for (const n of names) {
+// 		nameUpper.push(n.replace(n[0], n[0].toUpperCase()));
+// 	}
+
+// 	const capitalizeName = nameUpper.join(" ");
+
+// 	console.log(capitalizeName);
+// };
+
+// capitalizeName("jessica ann smith davis");
+// capitalizeName("jonas schmedtmann");
+
+// // Padding
+// const message = "Go to gate 23!";
+// console.log(message.padStart(25, "-").padEnd(25, "-"));
+// console.log("Jonas".padStart(25, "-").padEnd(25, "-"));
+
+// const maskCreditCard = function (number) {
+// 	const str = number + "";
+// 	const last = str.slice(-4);
+// 	return last.padStart(str.length, "*");
+// };
+
+// console.log(maskCreditCard(64637836));
+// console.log(maskCreditCard(43378463864647384));
+// console.log(maskCreditCard("334859493847755774747"));
+
+// // Repeat
+// const message2 = "Bad waether... All Departues Delayed... ";
+// console.log(message2.repeat(5));
+
+// const planesInLine = function (n) {
+// 	console.log(`There are ${n} planes in the line ${"🛩".repeat(n)}`);
+// };
+// planesInLine(5);
+// planesInLine(3);
+// planesInLine(12);
+
 // // Sets
 // const orderSet = new Set([
 // 	"Pasta",
@@ -540,37 +693,94 @@ The values are the events themselves, and the keys are the minutes in which each
 GOOD LUCK 😀
 */
 
-const gameEvents = new Map([
-	[17, "⚽️ GOAL"],
-	[36, "🔁 Substitution"],
-	[47, "⚽️ GOAL"],
-	[61, "🔁 Substitution"],
-	[64, "🔶 Yellow card"],
-	[69, "🔴 Red card"],
-	[70, "🔁 Substitution"],
-	[72, "🔁 Substitution"],
-	[76, "⚽️ GOAL"],
-	[80, "⚽️ GOAL"],
-	[92, "🔶 Yellow card"],
-]);
+// const gameEvents = new Map([
+// 	[17, "⚽️ GOAL"],
+// 	[36, "🔁 Substitution"],
+// 	[47, "⚽️ GOAL"],
+// 	[61, "🔁 Substitution"],
+// 	[64, "🔶 Yellow card"],
+// 	[69, "🔴 Red card"],
+// 	[70, "🔁 Substitution"],
+// 	[72, "🔁 Substitution"],
+// 	[76, "⚽️ GOAL"],
+// 	[80, "⚽️ GOAL"],
+// 	[92, "🔶 Yellow card"],
+// ]);
 
-console.log(typeof gameEvents);
+// console.log(typeof gameEvents);
 
-// 1.
-const events = [...new Set(gameEvents)];
-console.log(events);
+// // 1.
+// const events = [...new Set(gameEvents)];
+// console.log(events);
 
-// 2.
-gameEvents.delete(64);
-console.log(gameEvents);
+// // 2.
+// gameEvents.delete(64);
+// console.log(gameEvents);
 
-// 3.
-console.log(
-	`An event happened, on average, every ${90 / gameEvents.size} minutes`
-);
+// // 3.
+// console.log(
+// 	`An event happened, on average, every ${90 / gameEvents.size} minutes`
+// );
 
-//4.
-for (const [key, value] of gameEvents) {
-	const half = key <= 45 ? "FIRST" : "SECOND";
-	console.log(`[${half} HALF] ${key}: ${value}`);
-}
+// //4.
+// for (const [key, value] of gameEvents) {
+// 	const half = key <= 45 ? "FIRST" : "SECOND";
+// 	console.log(`[${half} HALF] ${key}: ${value}`);
+// }
+
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable 
+names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), 
+and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+first_name
+Some_Variable 
+calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. 
+		  Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, 
+		  so start watching the solution in case you're stuck. 
+		  Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+
+document.querySelector("button").addEventListener("click", function () {
+	const text = document.querySelector("textarea").value;
+	if (text) {
+		const texts = text.split("\n");
+
+		for (const [i, row] of texts.entries()) {
+			const [first, second] = row.toLowerCase().trim().split("_");
+
+			const output = `${first}${second.replace(
+				second[0],
+				second[0].toUpperCase()
+			)}`;
+
+			console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
+		}
+	}
+});
