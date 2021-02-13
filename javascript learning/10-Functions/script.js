02c5a29aae2c88011059f00a22b30a14030a6ec9
hoangghaii <nguyenhoanghai861@gmail.com>
2021-02-13 22:11:36 +0700

    .

--- javascript learning/10-Functions/script.js
+++ javascript learning/10-Functions/script.js
@@ -251,47 +251,155 @@ BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 GOOD LUCK 😀
 */
 
-const poll = {
-	question: "What is your favourite programming language?",
-	options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
-	// This generates [0, 0, 0, 0]. More in the next section 😃
-	answers: new Array(4).fill(0),
-	registerNewAnswer() {
-		const answer = Number(
-			prompt(
-				`${this.question}\n${this.options.join(
-					"\n"
-				)}\n(Write option number)!`
-			)
-		);
-		// console.log(answer);
+// const poll = {
+// 	question: "What is your favourite programming language?",
+// 	options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+// 	// This generates [0, 0, 0, 0]. More in the next section 😃
+// 	answers: new Array(4).fill(0),
+// 	registerNewAnswer() {
+// 		const answer = Number(
+// 			prompt(
+// 				`${this.question}\n${this.options.join(
+// 					"\n"
+// 				)}\n(Write option number)!`
+// 			)
+// 		);
+// 		// console.log(answer);
 
-		// Register answer
-		typeof answer === "number" &&
-			answer < this.answers.length &&
-			this.answers[answer]++;
+// 		// Register answer
+// 		typeof answer === "number" &&
+// 			answer < this.answers.length &&
+// 			this.answers[answer]++;
 
-		this.displayResults();
-		this.displayResults("string");
-	},
+// 		this.displayResults();
+// 		this.displayResults("string");
+// 	},
 
-	displayResults(type = "array") {
-		if (type === "array") {
-			console.log(this.answers);
-		} else if (type === "string") {
-			console.log(`Poll results are ${this.answers.join(", ")}`);
-		}
-	},
-};
+// 	displayResults(type = "array") {
+// 		if (type === "array") {
+// 			console.log(this.answers);
+// 		} else if (type === "string") {
+// 			console.log(`Poll results are ${this.answers.join(", ")}`);
+// 		}
+// 	},
+// };
 
-const registerNewAnswer = poll.registerNewAnswer.bind(poll);
-document.querySelector(".poll").addEventListener("click", registerNewAnswer);
+// const registerNewAnswer = poll.registerNewAnswer.bind(poll);
+// document.querySelector(".poll").addEventListener("click", registerNewAnswer);
 
-// poll.displayResults.call({ answers: [5, 2, 3] }, "string");
-// poll.displayResults.call({ answers: [5, 3, 9, 6, 1] }, "string");
-// poll.displayResults.call({ answers: [5, 2, 3] });
+// // poll.displayResults.call({ answers: [5, 2, 3] }, "string");
+// // poll.displayResults.call({ answers: [5, 3, 9, 6, 1] }, "string");
+// // poll.displayResults.call({ answers: [5, 2, 3] });
 
-const displayResults = poll.displayResults;
-displayResults.call({ answers: [5, 2, 3] }, "string");
-displayResults.call({ answers: [5, 3, 9, 6, 1] }, "string");
-displayResults.call({ answers: [5, 3, 9, 6, 1] });
+// const displayResults = poll.displayResults;
+// displayResults.call({ answers: [5, 2, 3] }, "string");
+// displayResults.call({ answers: [5, 3, 9, 6, 1] }, "string");
+// displayResults.call({ answers: [5, 3, 9, 6, 1] });
+
+///////////////////////////////////////
+// // Immediately Invoked Function Expressions (IIFE)
+// const runOnce = function () {
+// 	console.log("This will never run again");
+// };
+// runOnce();
+
+// // IIFE
+// (function () {
+// 	console.log("This will never run again");
+// 	const isPrivate = 23;
+// })();
+// // console.log(isPrivate);
+
+// (() => console.log("This will ALSO never run again"))();
+
+// {
+// 	const isPrivate = 23;
+// 	var notPrivate = 46;
+// }
+// // console.log(isPrivate);
+// console.log(notPrivate);
+
+///////////////////////////////////////
+// Closures
+// const secureBooking = function () {
+// 	let passengerCount = 0;
+
+// 	return function () {
+// 		passengerCount++;
+// 		console.log(`${passengerCount} passengers`);
+// 	};
+// };
+
+// const booker = secureBooking();
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+///////////////////////////////////////
+// More Closure Examples
+// Example 1
+// let f;
+
+// const g = function () {
+// 	const a = 23;
+// 	f = function () {
+// 		console.log(a * 2);
+// 	};
+// };
+
+// const h = function () {
+// 	const b = 777;
+// 	f = function () {
+// 		console.log(b * 2);
+// 	};
+// };
+
+// g();
+// f();
+// console.dir(f);
+
+// // Re-assigning f function
+// h();
+// f();
+// console.dir(f);
+
+// // Example 2
+// const boardPassengers = function (n, wait) {
+// 	const perGroup = n / 3;
+
+// 	setTimeout(function () {
+// 		console.log(`We are now boarding all ${n} passengers`);
+
+// 		console.log(`There are 3 groups, each with ${perGroup} passengers`);
+// 	}, wait * 1000);
+
+// 	console.log(`Will start boarding in ${wait} seconds`);
+// };
+
+// const perGroup = 1000;
+// boardPassengers(180, 3);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+	const h1 = document.querySelector("h1");
+	h1.style.color = "red";
+
+	const body = document.querySelector("body");
+	body.addEventListener("click", function () {
+		h1.style.color = "blue";
+	});
+})();
